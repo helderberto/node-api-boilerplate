ff193bd4795b4a0d50b9a322ccf8999a638c5121
Helder Burato Berto <helder.burato@gmail.com>
2020-04-11 12:45:10 -0300

    style: add .huskyrc to run git hooks before commit

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World');
-});
+routes.get('/', (req, res) => res.send('Hello World'));
 
 module.exports = routes;
