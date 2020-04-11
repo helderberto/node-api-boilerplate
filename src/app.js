13ab9a45092bba485e2c84b189274a6867a71ebc
Helder Burato Berto <helder.burato@gmail.com>
2020-04-11 12:28:56 -0300

    feat: server with express config and sample route in /

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const logger = require('morgan');
+const compression = require('compression');
+const routes = require('./routes');
+
+/**
+ * Load environment variables from .env file.
+ */
+dotenv.config({ path: '.env.example' });
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configs.
+ */
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Express load middlewares
+ */
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(compression());
+app.use(routes);
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log(
+    '🏁 Server is running on http://localhost:%s on %s mode',
+    app.get('port'),
+    app.get('env')
+  );
+  console.log('Press CTRL-C to stop\n');
+});
+
+module.exports = app;
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+  return res.send('Hello World');
+});
+
+module.exports = routes;
